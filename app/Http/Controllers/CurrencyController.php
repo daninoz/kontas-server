3d2b415e28977f8e9e6f04e1ec9e6339375735ff
Daniel Albornoz <daniel@daninoz.com>
2016-01-10 13:22:00 -0300

    Adds Currency Post

---
+++ app/Http/Controllers/CurrencyController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Currency;
+
+class CurrencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'unique:currencies', 'max:100'],
+            'exchange_rate' => ['required', 'numeric']
+        ]);
+
+        $currency = Currency::create([
+            'name' => $request->name,
+            'exchange_rate' => $request->exchange_rate
+        ]);
+
+        return response()->json(['id' => $currency->id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
